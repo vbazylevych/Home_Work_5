b3cce775f848d43a505efbb465b6668df33ffbc6
vbazylevych <vbazylevych@playtika.com>
2016-06-03 15:49:38 +0300

    2

--- src/goit/javaonline3/Runner.java
+++ src/goit/javaonline3/Runner.java
@@ -1,5 +1,6 @@
 package goit.javaonline3;
 
+import goit.javaonline3.arraysort.ArraySort;
 import goit.javaonline3.searchminandmax.SearchMinAndMax;
 
 import java.util.Scanner;
@@ -12,13 +13,12 @@ public class Runner {
     public static void main(String[] args){
 
         int arrayLength = 1;
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter array length");
         if(scanner.hasNextInt()) {
             arrayLength = scanner.nextInt();
             if(arrayLength > 0){
-                System.out.println("Enter " + arrayLength + " elements.");
+                System.out.println("Enter " + arrayLength + " elements of array.");
                 if(scanner.hasNextInt()) {
                     int[] array = new int[arrayLength];
                     for(int i = 0; i < arrayLength; i++) {
@@ -27,6 +27,8 @@ public class Runner {
                     SearchMinAndMax CustomArray = new SearchMinAndMax();
                     CustomArray.searchMin(array);
                     CustomArray.searchMax(array);
+                    ArraySort CustomArraySort= new ArraySort();
+                    CustomArraySort.sortArray(array);
                 }else {
                     System.out.println("You entered incorrect value of element. Please run program again");
                 }
--- src/goit/javaonline3/arraysort/ArraySort.java
+++ src/goit/javaonline3/arraysort/ArraySort.java
@@ -1,7 +1,26 @@
 package goit.javaonline3.arraysort;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by Ler4enko on 03.06.2016.
  */
 public class ArraySort {
+
+    public void sortArray(int[] array){
+
+       for(int j = array.length - 1; j > 0; j--){
+           for(int i = 0; i < j; i++){
+               if(array[i] > array[i + 1]) {
+                   int temp = array[i];
+                   array[i] = array[i + 1];
+                   array[i + 1] = temp;
+               }
+           }
+       }
+        System.out.println("Sorted array is:");
+        for(int element : array){
+            System.out.print(" " + element + " ");
+        }
+    }
 }
