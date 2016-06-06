4206c5b2ba3a6255dcd23f6562d3d9b71f358dee
vbazylevych <valeriyabazilevich@gmail.com>
2016-06-07 00:46:54 +0300

    add enterArray

--- src/goit/javaonline3/Runner.java
+++ src/goit/javaonline3/Runner.java
@@ -2,7 +2,6 @@ package goit.javaonline3;
 
 import goit.javaonline3.arraysort.WorkWithArray;
 
-import java.util.Scanner;
 
 /**
  * Created by Ler4enko on 03.06.2016.
@@ -10,38 +9,14 @@ import java.util.Scanner;
 public class Runner {
 
     public static void main(String[] args){
+        int[] array = WorkWithArray.enterArray();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter array length");
-
-        if (scanner.hasNextInt()) {
-            int arrayLength = scanner.nextInt();
-
-            if (arrayLength > 0) {
-                int[] array = new int[arrayLength];
-
-                System.out.println("Enter " + arrayLength + " elements of array.");
-
-                for (int i = 0; i < arrayLength; i++) {
-                    if (scanner.hasNextInt()) {
-                        array[i] = scanner.nextInt();
-                    } else {//http://www.oracle.com/technetwork/java/javase/documentation/codeconventions-142311.html
-                        System.out.println("You entered incorrect value of element. Please run program again");
-                        return;
-                    }
-                }
-
-                System.out.println("Min element is " + WorkWithArray.searchMin(array));
-                System.out.println("Max element is " + WorkWithArray.searchMax(array));
-                System.out.println("Sorted array is:");
-                array = WorkWithArray.sortArray(array);
-                WorkWithArray.printArray(array);
-            } else {//http://www.oracle.com/technetwork/java/javase/documentation/codeconventions-142311.html
-                System.out.println("You entered incorrect value of length. Please run program again!");
-            }
-        } else {
-            System.out.println("You entered incorrect value of length. Please run program again!");
+        if (array != null) {
+            System.out.println("Min element is " + WorkWithArray.searchMin(array));
+            System.out.println("Max element is " + WorkWithArray.searchMax(array));
+            System.out.println("Sorted array is:");
+            WorkWithArray.sortArray(array);
+            WorkWithArray.printArray(array);
         }
     }
 }
--- src/goit/javaonline3/arraysort/WorkWithArray.java
+++ src/goit/javaonline3/arraysort/WorkWithArray.java
@@ -2,6 +2,8 @@ package goit.javaonline3.arraysort;
 //зачем этот импорт?
 
 
+import java.util.Scanner;
+
 /**
  * Created by Ler4enko on 03.06.2016.
  */
@@ -9,6 +11,39 @@ public class WorkWithArray {
     //что если сделать эти методы статическими?
     //Видя название sortArray, я бы подумала что этот метод вернет мне отсортированный массив.
     //это было бы логично - мотод возвращает нам массив и мы уже потом что хотим, то с ним и делаем
+    public static int[] enterArray() {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] array;
+
+        System.out.println("Enter array length");
+        if (scanner.hasNextInt()) {
+            int arrayLength = scanner.nextInt();
+
+            if (arrayLength > 0) {
+                array = new int[arrayLength];
+                System.out.println("Enter " + arrayLength + " elements of array.");
+
+                for (int i = 0; i < arrayLength; i++) {
+                    if (scanner.hasNextInt()) {
+                        array[i] = scanner.nextInt();
+                    } else {//http://www.oracle.com/technetwork/java/javase/documentation/codeconventions-142311.html
+                        System.out.println("You entered incorrect value of element. Please run program again");
+                        array = null;
+                        break;
+                    }
+                }
+            } else {//http://www.oracle.com/technetwork/java/javase/documentation/codeconventions-142311.html
+                System.out.println("You entered incorrect value of length. Please run program again!");
+                array = null;
+            }
+        } else {
+            System.out.println("You entered incorrect value of length. Please run program again!");
+            array = null;
+        }
+        return array;
+    }
+
     public static int[] sortArray(int[] array){
 
        for (int j = array.length - 1; j > 0; j--) {
@@ -22,28 +57,28 @@ public class WorkWithArray {
        }
        return array;
     }
-        //что если сделать эти методы статическими?
-        public static int searchMin(int[] array){
-            int min = array[0];
 
-            for (int i = 1; i < array.length ; i++) {
-                if (min > array[i]) {
-                    min = array[i];
-                }
+    public static int searchMin(int[] array){
+        int min = array[0];
+
+        for (int i = 1; i < array.length ; i++) {
+            if (min > array[i]) {
+                min = array[i];
             }
-            return min;
         }
+        return min;
+    }
 
-        public static int searchMax(int[] array){
-            int max = array[0];
+    public static int searchMax(int[] array){
+        int max = array[0];
 
-            for (int i = 1; i < array.length; i++) {
-                if (max < array[i]) {
-                    max = array[i];
-                }
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
             }
-            return max;
         }
+        return max;
+    }
 
     public static void printArray(int[] array) {
         for (int element : array) {
